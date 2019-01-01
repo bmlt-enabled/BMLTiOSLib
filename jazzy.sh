8a2f0252c94dea7d4c142772600f4d488d796676
Chris M <webmaster@magnaws.com>
2019-01-01 08:12:27 -0500

    Updated the Jazzy script to point to the correct URI. This is way too small a change for a new version by itself.

--- jazzy.sh
+++ jazzy.sh
@@ -2,6 +2,6 @@
 CWD="$(pwd)"
 MY_SCRIPT_PATH=`dirname "${BASH_SOURCE[0]}"`
 cd "${MY_SCRIPT_PATH}"
-jazzy --github_url https://github.com/LittleGreenViper/BMLTiOSLib --readme ./README.md
+jazzy --github_url https://github.com/bmlt-enabled/BMLTiOSLib --readme ./README.md
 cp icon.png docs/
 cd "${CWD}"
