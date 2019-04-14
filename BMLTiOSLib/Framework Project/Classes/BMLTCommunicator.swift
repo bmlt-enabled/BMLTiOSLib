6676c544af082ad4fac126b510e97f45cf0ec5b1
Chris Marshall <chris@riftvalleysoftware.com>
2019-04-13 22:45:40 -0400

    Fixed a Swift 5 warning.

--- BMLTiOSLib/Framework Project/Classes/BMLTCommunicator.swift
+++ BMLTiOSLib/Framework Project/Classes/BMLTCommunicator.swift
@@ -322,9 +322,11 @@ class BMLTSession: NSObject, URLSessionDataDelegate, BMLTCommunicatorDataSourceP
         if (inSession == self.mySession) && (nil != self.myCurrentTask) {  // Make sure this is us.
             // This weird dance is because there's funky stuff going on under the hood, and this
             // will ensure that we have a separate, new copy of the data.
-            didReceive.withUnsafeBytes {(bytes: UnsafePointer<CChar>) -> Void in
-                let myDataObject: Data = Data(bytes: bytes, count: didReceive.underestimatedCount)
-                self.myCurrentTask.data.append(myDataObject)
+            didReceive.withUnsafeBytes {(bytes: UnsafeRawBufferPointer) -> Void in
+                if let baseAddr = bytes.baseAddress {
+                    let myDataObject = Data(bytes: baseAddr, count: didReceive.underestimatedCount)
+                    self.myCurrentTask.data.append(myDataObject)
+                }
             }
         }
     }
