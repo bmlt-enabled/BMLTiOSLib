645c5986ff775ef4f48ae22cd70c28047bd2bd3a
Chris Marshall <chris@riftvalleysoftware.com>
2019-08-01 17:46:26 -0400

    Updated the Jazzy file to indicate BMLT-Enabled as the author.

--- jazzy.sh
+++ jazzy.sh
@@ -4,8 +4,8 @@ MY_SCRIPT_PATH=`dirname "${BASH_SOURCE[0]}"`
 cd "${MY_SCRIPT_PATH}"
 jazzy   --github_url https://github.com/bmlt-enabled/BMLTiOSLib\
         --readme ./README.md --theme fullwidth\
-        --author Little\ Green\ Viper\ Software\ Development\ LLC\
-        --author_url https://littlegreenviper.com\
+        --author BMLT-Enabled\
+        --author_url https://bmlt.app\
         --min-acl public
 cp icon.png docs/
 cd "${CWD}"
